2938af57e5826ea1267674117960dfc72a96c9e5
Garulf <535299+Garulf@users.noreply.github.com>
2022-01-09 13:25:34 -0500

    init

---
+++ source/FlowLauncherExporter.psm1
@@ -0,0 +1,44 @@
+function ExportLibrary()
+{
+    param(
+        $scriptMainMenuItemActionArgs
+    )
+
+    $path = $PlayniteApi.Paths.ExtensionsDataPath
+    $full_path = Join-Path -Path $path -ChildPath "FlowLauncher_Exporter" | Join-Path -ChildPath "library.csv"
+    $PlayniteApi.Database.Games | Select Id, Name, Source, ReleaseDate, Playtime, IsInstalled, InstallDirectory, Icon | ConvertTo-Csv | Out-File $full_path -Encoding utf8
+
+}
+
+function OnLibraryUpdated()
+{
+    ExportLibrary
+}
+
+function OnApplicationStarted()
+{
+    ExportLibrary
+}
+
+function OnGameInstalled 
+{
+    ExportLibrary
+}
+
+function OnGameUninstalled 
+{
+    ExportLibrary
+}
+
+function GetMainMenuItems()
+{
+    param(
+        $menuArgs
+    )
+
+    $menuItem = New-Object Playnite.SDK.Plugins.ScriptMainMenuItem
+    $menuItem.Description = "Update index"
+    $menuItem.FunctionName = "ExportLibrary"
+    $menuItem.MenuSection = "@Flow Launcher Exporter"
+    return $menuItem
+}
